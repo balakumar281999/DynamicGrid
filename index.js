7e1d7ab55cb6636ec21715e9ca472fda770db62e
balakumar281999 <121997496+balakumar281999@users.noreply.github.com>
2023-03-02 18:24:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,336 +1,146 @@
-function navSlide() {
-    var burger = document.querySelector(".burger");
-    var nav = document.querySelector(".nav-links");
-  
-    burger.addEventListener("click", function() {
-      nav.classList.toggle("nav-active");
-      burger.classList.toggle("toggle");
-    });
-  }
-  navSlide();
-  
-  
-  $(document).ready(function() {
-    $("#bannersection").slick({
-      dots: true,
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false
-    });
-  });
-  
-  var clothingCards = document.getElementById("clothingCards");
-  var accessoriesCards = document.getElementById("accessoriesCards");
-  
-  function createItemCard(id, preview, name, brand, price) {
-  
-    var cardElement = document.createElement("div");
-    cardElement.setAttribute("class", "card");
-    cardElement.setAttribute("id", id);
-  
-    var cardLink = document.createElement("a");
-    cardLink.href = "product.html?product_id=" + id;
-  
-    var imgContainer = document.createElement("div");
-    imgContainer.setAttribute("class", "img");
-  
-    var newImgElement = document.createElement("img");
-    newImgElement.src = preview;
-  
-    imgContainer.appendChild(newImgElement);
-  
-   var deatils = document.createElement("div");
-    deatils.setAttribute("class", "details");
-  
-   var newTitleElement = document.createElement("h3");
-    var newName = document.createTextNode(name);
-  
-   newTitleElement.appendChild(newName);
-    deatils.appendChild(newTitleElement);
-  
-   var newBrandElement = document.createElement("h4");
-    var newBrand = document.createTextNode(brand);
-  
-   newBrandElement.appendChild(newBrand);
-    deatils.appendChild(newBrandElement);
-  
-   var newPriceElement = document.createElement("h5");
-    var newPriceText = document.createTextNode("Rs ");
-    var newPrice = document.createTextNode(price);
-    newPriceElement.appendChild(newPriceText);
-  
-     newPriceElement.appendChild(newPrice);
-    deatils.appendChild(newPriceElement);
-  
-      cardLink.appendChild(imgContainer);
-    cardLink.appendChild(deatils);
-  
-      cardElement.appendChild(cardLink);
-  
-      return cardElement;
-  }
-  
-  
-  
-  function getCardsData() {
-    $.get("https://5d76bf96515d1a0014085cf9.mockapi.io/product", function(data) {
-      var responseData = data;
-      for (var i = 0; i < responseData.length; i++) {
-        if (responseData[i].isAccessory === false) {
-          clothingCards.append(
-            createItemCard(
-              responseData[i].id,
-              responseData[i].preview,
-              responseData[i].name,
-              responseData[i].brand,
-              responseData[i].price
-            )
-          );
-        } else {
-          accessoriesCards.append(
-            createItemCard(
-              responseData[i].id,
-              responseData[i].preview,
-              responseData[i].name,
-              responseData[i].brand,
-              responseData[i].price
-            )
-          );
-        }
-      }
-    });
-  }
-  getCardsData();
-  
-    
-  function getProductDetail() {
-  
-    var searchId = window.location.search.split("=")[1];
-  
-   $.get(
-      "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + searchId,
-      function(data) {
-        var productDetail = data;
-        name = productDetail.name;
-        imageSrc = productDetail.preview;
-        brand = productDetail.brand;
-        price = productDetail.price;
-        description = productDetail.description;
-        photo0 = productDetail.photos[0];
-        photo1 = productDetail.photos[1];
-        photo2 = productDetail.photos[2];
-        photo3 = productDetail.photos[3];
-        photo4 = productDetail.photos[4];
-        photo5 = productDetail.photos[5];
-  
-        createProductPage(
-          imageSrc,
-          name,
-          brand,
-          price,
-          description,
-          photo0,
-          photo1,
-          photo2,
-          photo3,
-          photo4,
-          photo5
-        );
-      }
-    );
-  }
-  getProductDetail();
-  
-  
-  function createProductPage(
-    imageSrc,
-    name,
-    brand,
-    price,
-    description,
-    img0,
-    img1,
-    img2,
-    img3,
-    img4,
-    img5
-  ) {
-     var productImg = document.getElementById("productImg");
-    productImg.src = imageSrc;
-  
-    var productName = document.getElementById("name");
-    productName.innerHTML = name;
-  
-    var productBrand = document.getElementById("brand");
-    productBrand.innerHTML = brand;
-    
-    var productPrice = document.getElementById("price");
-    productPrice.innerHTML = price;
-  
-  
-    var productDescription = document.getElementById("description");
-    productDescription.innerHTML = description;
-  
-  
-    var photo0 = document.getElementById("img0");
-    photo0.src = img0;
-  
-    var photo1 = document.getElementById("img1");
-    photo1.src = img1;
-  
-    
-    var photo2 = document.getElementById("img2");
-    photo2.src = img2;
-  
-  
-    var photo3 = document.getElementById("img3");
-    photo3.src = img3;
-  
-    var photo4 = document.getElementById("img4");
-    photo4.src = img4;
-  
-   
-    var photo5 = document.getElementById("img5");
-    photo5.src = img5;
-  
+const content=document.body
+let product01 = document.querySelector('.clothing-container')
 
-    function changeImage() {
-   
-      photo0.addEventListener("click", function() {
-        productImg.src = img0;
-      });
-  
-    
-      photo1.addEventListener("click", function() {
-        productImg.src = img1;
-      });
-     
-      photo2.addEventListener("click", function() {
-        productImg.src = img2;
-      });
-  
-      
-      photo3.addEventListener("click", function() {
-        productImg.src = img3;
-      });
-  
-    
-      photo4.addEventListener("click", function() {
-        productImg.src = img4;
-      });
-  
-  
-      photo5.addEventListener("click", function() {
-        productImg.src = img5;
-      });
-  
-     
-      $(document).on("click", ".previewImg img", function() {
-        $(this)
-          .addClass("active")
-          .siblings()
-          .removeClass("active");
-      });
-    }
-    changeImage();
-  }
-  
- 
-  
-  var addToCartBtn = document.getElementById("add-to-cart");
-  var cart = document.getElementById("cart-count");
-  var myCartData = [];
-  var cartIntialValue;
-  
-  if(localStorage.getItem('cart-count') == null) {
-      localStorage.setItem('cart-count', '0');
-  } else {
-      var cartValue = localStorage.getItem('cart-count');
-      localStorage.setItem('cart-count', cartValue);
-  }
-  
-  
-  
-  function cartCount() {
-    if (window.localStorage.getItem("cart-count") === null) {
-      cartIntialValue = 0;
-    } else {
-      cartIntialValue = JSON.parse(window.localStorage.getItem("cart-count"));
-      cart.innerHTML = cartIntialValue;
-    }
-    var cartCurrentValue = cartIntialValue + 1;
-    window.localStorage.setItem("cart-count", cartCurrentValue);
-    cart.innerHTML = window.localStorage.getItem("cart-count");
-  }
-  cart.innerHTML = window.localStorage.getItem("cart-count");
-  
-  
-  function addDataIntoList(productData) {
 
-    if (window.localStorage.getItem("product-list") === null) {
-      myCartData = [];
+
+let productlist1 = [{
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+        name: "Men Navy Blue Solid Sweatshirt",
+        brand: "United Colors of Benetton",
+        price: "Rs 2599"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2060340/2017/9/14/11505387708574-Puma-Men-Black-Solid-Sporty-Jacket-371505387708496-1.jpg",
+        name: "Men Black MAMGP T7 Sweat Sporty Jacket",
+        brand: "Puma",
+        price: "Rs 7999"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2018/9/19/b1373b00-37d4-41a0-a908-5d742a68d3661537343756229-1.jpg",
+        name: "Men Black Action Parkview Lifestyle Shoes",
+        brand: "Hush Puppies",
+        price: "Rs 6999"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/3854721/2018/3/12/11520844673915-BARESKIN-Women-Black-Solid-Lightweight-Leather-Jacket-7761520844673718-1.jpg",
+        name: "Women Black Solid Lightweight Leather Jacket",
+        brand: "BARESKIN",
+        price: "Rs 9999"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/5649908/2018/5/10/6bfe80cd-2f55-42bc-aa7f-e0d6c9e2ac531525936414747-SASSAFRAS-Women-Blue-Solid-Shirt-Dress-3831525936414532-1.jpg",
+        name: "Women Blue Solid Shirt Dress",
+        brand: "SASSAFRAS",
+        price: "Rs 5200"
     }
-    else {
-      myCartData = JSON.parse(window.localStorage.getItem("product-list"));
+]
+
+function cardbox1(assign01) {
+    let card01 = document.createElement("div");
+    card01.classList.add("card");
+    product01.appendChild(card01);
+
+    let image01 = document.createElement("img");
+    image01.classList.add("card-img-top");
+    image01.src = assign01.preview;
+    card01.appendChild(image01);
+
+    let headingE = document.createElement("h3");
+    headingE.classList.add("heading02");
+    headingE.textContent = assign01.name;
+    card01.appendChild(headingE);
+
+
+    let headinge = document.createElement("h4");
+    headinge.classList.add("heading03");
+    headinge.textContent = assign01.brand;
+    card01.appendChild(headinge);
+
+
+    let headingU = document.createElement("h5");
+    headingU.classList.add("heading04");
+    headingU.textContent = assign01.price;
+    card01.appendChild(headingU);
+
+}
+
+
+for (let assign01 of productlist1) {
+    cardbox1(assign01)
+}
+
+
+
+
+
+
+
+let product02= document.querySelector('.Accessories-container')
+
+
+
+let productlist2 = [{
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803247/2019/5/27/624dac60-4c42-4902-bba1-30a51cc7f43c1558948536543-Apple-Unisex-Smart-Watches-1441558948536225-1.jpg",
+        name: "Unisex Silver-Toned Series 3 Smart Watch",
+        brand: "Apple",
+        price: "Rs 31999"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2019/2/23/dc05b5e5-aa82-493e-bf0b-0c309baf21021550920383101-1.jpg",
+        name: "Unisex Black & Green Reflex 2.0 Smart Band",
+        brand: "Fastrack",
+        price: "Rs 1999"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/10122083/2019/6/24/e533a691-3908-41b0-8307-1928a37d4ec41561362162650-Samsung-Galaxy-Fit-4801561362161527-1.jpg",
+        name: "Unisex Black Galaxy Fit Fitness Band",
+        brand: "Samsung",
+        price: "Rs 9990"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/8593217/2019/4/9/de2c21f3-6731-4c29-9369-692a486a1b8c1554802772926-Samsung-Gear-IconX-Black-Cord-free-Fitness-Earbuds-256155480-1.jpg",
+        name: "Gear IconX Black Cord-free Fitness Earbuds",
+        brand: "Samsung",
+        price: "Rs 13990"
+    },
+    {
+        preview: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803279/2019/5/27/6661d579-12ae-456b-b8f2-e78e5256a99c1558948436173-Apple-AirPods-with-Charging-Case-2nd-Gen-White-3831558948435-1.jpg",
+        name: "White 2nd Gen AirPods with Charging Case",
+        brand: "Apple",
+        price: "Rs 14999"
     }
-    if (myCartData.length === 0) {
-      var myObj = {
-        id: productData.id,
-        title: productData.name,
-        count: 1,
-        price: productData.price,
-        preview: productData.preview
-      };
-      myCartData.push(myObj);
-    }
-    else if (myCartData.length != 0) {
-      var w = 0;
-      for (var i = 0; i < myCartData.length; i++) {
-        if (myCartData[i].id == productData.id) {
-          myCartData[i].count = parseInt(myCartData[i].count) + 1;
-          w += 1;
-        }
-      }
-  
-      if (w == 0) {
-        var myObj = {
-          id: productData.id,
-          title: productData.name,
-          count: 1,
-          price: productData.price,
-          preview: productData.preview
-        };
-        myCartData.push(myObj);
-      }
-    }
-   
-    window.localStorage.setItem("product-list", JSON.stringify(myCartData));
-  }
-  
- 
-  
-  addToCartBtn.addEventListener("click", function() {
-    var productId = window.location.search.split("=")[1];
-    var urlLink =
-      "https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + productId;
-  
-    function getDataForLocalStorage() {
-      var http = new XMLHttpRequest();
-      http.onreadystatechange = function() {
-        if (this.readyState === 4) {
-          if (this.status === 200) {
-            var productData = JSON.parse(this.responseText);
-            addDataIntoList(productData);
-          }
-        }
-      };
-      http.open("GET", urlLink, true);
-      http.send();
-    }
-    cartCount();
-    getDataForLocalStorage();
-  });
-  
+]
+
+function cardbox2(assign02) {
+  let card01 = document.createElement("div");
+  card01.classList.add("card");
+  product02.appendChild(card01);
+
+  let image01 = document.createElement("img");
+  image01.classList.add("card-img-top");
+  image01.src = assign02.preview;
+  card01.appendChild(image01);
+
+  let headingE = document.createElement("h3");
+  headingE.classList.add("heading02");
+  headingE.textContent = assign02.name;
+  card01.appendChild(headingE);
+
+
+  let headinge = document.createElement("h4");
+  headinge.classList.add("heading03");
+  headinge.textContent = assign02.brand;
+  card01.appendChild(headinge);
+
+
+  let headingU = document.createElement("h5");
+  headingU.classList.add("heading04");
+  headingU.textContent = assign02.price;
+  card01.appendChild(headingU);
+
+}
+
+
+for (let assign02 of productlist2) {
+    cardbox2(assign02)
+}
